cfa30493f87387d1932affd5a99e5d38d124a2e2
Alan-N-Pereira <itsalanpereira@gmail.com>
2021-12-09 22:20:29 +0530

    update footer

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -36,6 +36,8 @@ export function FooterContainer() {
 					<Footer.Link href="#">Legal Notices</Footer.Link>
 				</Footer.Column>
 			</Footer.Row>
+			<Footer.Break />
+			<Footer.Text>Netflix India</Footer.Text>
 		</Footer>
 	);
 }
