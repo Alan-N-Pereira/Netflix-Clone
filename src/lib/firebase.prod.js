9537934237afdb04aa27726a98ce74d22475acf9
Alan-N-Pereira <itsalanpereira@gmail.com>
2021-12-26 23:57:15 +0530

    Add initial firebase connection

---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,12 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// seed the database
+
+// config
+const config = {};
+
+const firebase = Firebase.initializeApp(config);
+
+export { firebase };
