8f54a85ed21dda17b8314d14f9d7fc41bd903728
Alan-N-Pereira <itsalanpereira@gmail.com>
2021-12-29 23:00:57 +0530

    Add firebase connection and seeded data into cluster

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'normalize.css';
 import { GlobalStyles } from './globalStyles';
+// eslint-disable-next-line
+import { firebase } from './lib/firebase.prod';
 
 ReactDOM.render(
 	<div>
--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,12 +1,22 @@
-import Firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import Firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+// import { seedDatabase } from '../seed';
 
 // seed the database
 
 // config
-const config = {};
+const config = {
+	// apiKey: REACT_APP_API_KEY,
+	// authDomain: REACT_APP_AUTH_DOMAIN,
+	// projectId: REACT_APP_PROJECT_ID,
+	// storageBucket: REACT_APP_STORAGE_BUCKET,
+	// messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+	// appId: REACT_APP_APP_ID
+};
 
 const firebase = Firebase.initializeApp(config);
+
+// seedDatabase(firebase);
 
 export { firebase };
