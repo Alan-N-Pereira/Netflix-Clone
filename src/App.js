1a1e794cf4472c943e818920d29f8b658934529b
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-01-08 23:59:32 +0530

    Add Signup redirect

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { Home, Browse, Signin, Signup } from './pages/index';
 import * as ROUTES from './constants/routes';
+import { IsUserRedirect } from './helpers/routes';
 
 function App() {
+	const user = {};
 	return (
 		<Router>
 			<Routes>
 				<Route path={ROUTES.HOME} element={<Home />} />
 				<Route path={ROUTES.BROWSE} element={<Browse />} />
 				<Route path={ROUTES.SIGN_UP} element={<Signup />} />
-				<Route path={ROUTES.SIGN_IN} element={<Signin />} />
+				<Route
+					path={ROUTES.SIGN_IN}
+					element={
+						<IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
+							<Signin />
+						</IsUserRedirect>
+					}
+				/>
 			</Routes>
 		</Router>
 	);
--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -7,12 +7,12 @@ import 'firebase/compat/auth';
 
 // config
 const config = {
-	// apiKey: process.env.REACT_APP_API_KEY,
-	// authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	// projectId: process.env.REACT_APP_PROJECT_ID,
-	// storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-	// messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-	// appId: process.env.REACT_APP_APP_ID
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID
 };
 
 const firebase = initializeApp(config);
