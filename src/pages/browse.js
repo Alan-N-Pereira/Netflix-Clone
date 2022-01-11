7e701c4fcc058fe66824bbd101fb7fdc811ecdc5
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-01-11 23:39:28 +0530

    Add utils filter for genre title

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { useContent } from '../hooks';
+import selectionFilter from '../utils/selection-filter';
 
 export default function Browse() {
 	const { series } = useContent('series');
 	const { films } = useContent('films');
+
+	const slides = selectionFilter({ series, films });
 
 	return <p>Hello from browse</p>;
 }
