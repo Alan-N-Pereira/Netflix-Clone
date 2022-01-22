25ed4d3d5d24d3f8554a183c2f67eac401f0b6e7
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-01-22 23:59:10 +0530

    Update Loading component

--- src/components/profiles/index.js
+++ src/components/profiles/index.js
@@ -18,7 +18,7 @@ Profiles.User = function ProfilesUser({ children, ...restProps }) {
 };
 
 Profiles.Picture = function ProfilesPicture({ src, ...restProps }) {
-	return <Picture {...restProps} src={src ? `/images/users/${src}.png` : `/images/misc/loading`} />;
+	return <Picture {...restProps} src={src ? `/images/users/${src}.png` : `/images/misc/loading.gif`} />;
 };
 
 Profiles.Name = function ProfilesName({ children, ...restProps }) {
