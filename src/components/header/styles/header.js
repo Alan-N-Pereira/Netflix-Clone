e76a97001204c50ea489b609149dad28670a2128
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-01-25 23:59:39 +0530

    Update Btowse container

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -6,7 +6,7 @@ import { Link as ReactRouterLink } from 'react-router-dom';
 export const Background = styled.div`
 	display: flex;
 	flex-direction: column;
-	background: url(${({ src }) => (src ? '../images/misc/${src}.jpg' : '../images/misc/home-bg.jpg')}) top left / cover
+	background: url(${({ src }) => (src ? `../images/misc/${src}.jpg` : `../images/misc/home-bg.jpg`)}) top left / cover
 		no-repeat;
 `;
 
@@ -56,4 +56,33 @@ export const ButtonLink = styled(ReactRouterLink)`
     &:hover{
         background-color:#f40612;
     }
+`;
+
+export const Feature = styled(Container)`
+padding: 150px 0 500px 0;
+flex-direction:column;
+align-items:normal;
+width:50%;
+
+@media (max-width: 1100px){
+	display:none;
+}
+`;
+
+export const Text = styled.p`
+	color: white;
+	font-size: 22px;
+	line-height: normal;
+	text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+	margin: 0;
+`;
+
+export const FeatureCallOut = styled.h2`
+	color: white;
+	font-size: 50px;
+	line-height: normal;
+	font-weight: bold;
+	text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+	margin: 0;
+	margin-bottom: 20px;
 `;
