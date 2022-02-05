4ead77f2adba37a0c6d35040d68c09c3443e3668
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-02-05 22:46:30 +0530

    Add Search in Browse container header

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -6,6 +6,7 @@ import * as ROUTES from '../constants/routes';
 import logo from '../logo.svg';
 
 export default function BrowseContainer({ slides }) {
+	const [ searchTerm, setSearchTerm ] = useState('');
 	const [ profile, setProfile ] = useState({});
 	const [ loading, setLoading ] = useState(true);
 	const auth = getAuth();
@@ -31,6 +32,7 @@ export default function BrowseContainer({ slides }) {
 						<Header.TextLink>Films</Header.TextLink>
 					</Header.Group>
 					<Header.Group>
+						<Header.Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
 						<Header.Profile>
 							<Header.Picture src={user.photoURL} />
 							<Header.Dropdown>
