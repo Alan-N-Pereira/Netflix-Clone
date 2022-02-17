58df506320beac418e5083f7368a01263eaa7d52
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-02-17 23:46:13 +0530

    Add Player Component

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -58,7 +58,7 @@ Card.Feature = function CardFeature({ children, category, ...restProps }) {
 	const { showFeature, itemFeature, setShowFeature } = useContext(FeatureContext);
 
 	return showFeature ? (
-		<Feature src={`/images/${category}/${itemFeature.genre}/${itemFeature.slug}/large.jpg`}>
+		<Feature {...restProps} src={`/images/${category}/${itemFeature.genre}/${itemFeature.slug}/large.jpg`}>
 			<Content>
 				<FeatureTitle>{itemFeature.title}</FeatureTitle>
 				<FeatureText>{itemFeature.description}</FeatureText>
---
+++ src/components/player/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+import {} from './styles/player';
+export default function Player() {
+	return <div>Player</div>;
+}
