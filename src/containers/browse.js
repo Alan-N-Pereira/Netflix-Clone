50c6ca4f864e303f702f09c3e517da5338335946
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-02-20 23:56:20 +0530

    Add Filter Search using Fuse.js

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Fuse from 'fuse.js';
 import { SelectProfileContainer } from './profiles';
 import { getAuth, signOut } from 'firebase/auth';
 import { Loading, Header, Card, Player } from '../components';
@@ -30,6 +31,20 @@ export default function BrowseContainer({ slides }) {
 			setSlideRows(slides[category]);
 		},
 		[ slides, category ]
+	);
+
+	useEffect(
+		() => {
+			const fuse = new Fuse(slideRows, { keys: [ 'data.description', 'data.title', 'data.genre' ] });
+			const results = fuse.search(searchTerm).map(({ item }) => item);
+
+			if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+				setSlideRows(results);
+			} else {
+				setSlideRows(slides[category]);
+			}
+		},
+		[ searchTerm ]
 	);
 
 	return profile.displayName ? (
